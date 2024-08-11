e4fb38d1182961879b73d103d958448a06170a45
sandarunihara <sandarunihara56@gmail.com>
2024-08-11 19:04:39 +0530

    1

---
+++ q1.scala
@@ -0,0 +1,20 @@
+def encrypt(text: String, shift: Int): String = {
+    text.map { char =>
+      if (char.isLetter) {
+        val base = if (char.isUpper) 'A' else 'a'
+        ((char - base + shift) % 26 + base).toChar
+      } else {
+        char
+      }
+    }
+  }
+
+
+def decrypt(text: String, shift: Int): String = {
+  encrypt(text, 26 - shift) 
+}
+
+def cipher(text: String, shift: Int, operation: (String, Int) => String): String = {
+    operation(text, shift)
+  }
+
